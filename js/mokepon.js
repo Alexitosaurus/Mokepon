921b2e1f180676e338e96ac30d74843be864b320
Alex Daniel <alexitosauriorex@hotmail.com>
2026-03-31 20:33:26 -0600

    ahora tiene seccion de seleccion al azar de enemigo

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,48 +1,100 @@
-
 function iniciarjuego () {
-
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 
-
-
-
-}
-
-
-
-function seleccionarMascotaJugador () {
-    //alert("seleccionaste tu mascota")
-
- let mascota_seleccionada
-
-    if (document.getElementById('Hipodogue').checked == true) {
-
-        alert("seleccionaste hipodogue")
-        let mascota_seleccionada = 'hipodogue'
-
-    } else if (document.getElementById('Capipepo').checked == true) {
-
-        alert("seleccionaste capipepo")
-        let mascota_seleccionada = 'capipepo'
-
-    } else if (document.getElementById('Ratigueya').checked == true) {
-
-        alert("seleccionaste ratigueya")
-        let mascota_seleccionada = 'ratigueya'
-
-    } else {
-
-        alert("selecciona una mascota")
-
-    }
+     let botonMascotaEnemigo = document.getElementById('boton-mascota')
+    botonMascotaEnemigo.addEventListener('click', seleccionarMascotaEnemigo)
     
 }
 
+function seleccionarMascotaJugador () 
+{
+    //alert("seleccionaste tu mascota")
+ 
+ let mascota_seleccionada
+    
+    if (document.getElementById('Hipodogue').checked == true) {
+
+        
+        let mascota_seleccionada = 'hipodogue'
+        document.getElementById('nombre-mascota').innerHTML = mascota_seleccionada
+       
+    } else if (document.getElementById('Capipepo').checked == true) {
+
+       
+        let mascota_seleccionada = 'capipepo'
+        document.getElementById('nombre-mascota').innerHTML = mascota_seleccionada
+
+    } else if (document.getElementById('Ratiguella').checked == true) {
+
+        
+        let mascota_seleccionada = 'ratiguella'
+        document.getElementById('nombre-mascota').innerHTML = mascota_seleccionada
+
+    } else if (document.getElementById('Langostelvis').checked == true) {
+
+        let mascota_seleccionada = 'langostelvis'
+        document.getElementById('nombre-mascota').innerHTML = mascota_seleccionada
+
+    } else if (document.getElementById('Tucapalma').checked == true) {
+
+        
+        let mascota_seleccionada = 'tucapalma'
+        document.getElementById('nombre-mascota').innerHTML = mascota_seleccionada
+
+    } else if (document.getElementById('Pydos').checked == true) {
+
+        
+        let mascota_seleccionada = 'pydos'
+        document.getElementById('nombre-mascota').innerHTML = mascota_seleccionada
+    }   
+    else {
+
+        alert("selecciona una mascota")
+
+         }
 
 
+}
+
+function seleccionarMascotaEnemigo () {
+
+        let mascotaAleatoria = aleatorio(1,6)
+
+        let mascota_enemigo
+
+        if (mascotaAleatoria == 1) {
+
+            let mascota_enemigo = 'hipodogue'
+             document.getElementById('mascotamala').innerHTML = mascota_enemigo
+       
+
+        } else if (mascotaAleatoria == 2) { 
+            let mascota_enemigo = 'capipepo'
+             document.getElementById('mascotamala').innerHTML = mascota_enemigo         
+
+        } else if (mascotaAleatoria == 3) {
+            let mascota_enemigo = 'ratiguella'
+             document.getElementById('mascotamala').innerHTML = mascota_enemigo 
+        } else if (mascotaAleatoria == 4) {
+            let mascota_enemigo = 'langostelvis'
+             document.getElementById('mascotamala').innerHTML = mascota_enemigo     
+
+        } else if (mascotaAleatoria == 5) {
+            let mascota_enemigo = 'tucapalma'
+             document.getElementById('mascotamala').innerHTML = mascota_enemigo
 
 
+        } else if (mascotaAleatoria == 6) {
+            let mascota_enemigo = 'pydos'
+             document.getElementById('mascotamala').innerHTML = mascota_enemigo 
 
+        }
+
+        function aleatorio (min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min)    
+        }
+
+        }
 
 window.addEventListener('load', iniciarjuego)
